6e544618e5cdf1f2c8d78d1d536757d48dd86b66
Kiran Kelageri <kirankelageri@codeaurora.org>
2015-08-04 15:06:22 -0700

    ANT: Enable ANT feature.
    Enabling ANT feature.
    
    Change-Id: Ic11293020a9d626f57adb6ea37d49306fca5962c

--- libbt-vendor/include/bt_vendor_qcom.h
+++ libbt-vendor/include/bt_vendor_qcom.h
@@ -42,6 +42,10 @@ typedef enum {
     BT_SOC_RESERVED
 }bt_soc_type;
 
+typedef enum {
+    BT_VND_OP_ANT_USERIAL_OPEN = 254,
+    BT_VND_OP_ANT_USERIAL_CLOSE
+}ant_serial;
 
 extern bt_vendor_callbacks_t *bt_vendor_cbacks;
 /* HW_NEED_END_WITH_HCI_RESET
