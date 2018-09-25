347ab71f7ddcabe8d0c74286a1517aa230448d1d
Leo Jia <leo.jia@oneplus.com>
2018-09-25 11:48:25 +0800

    y[845]:[system] Add for community
    Plan: as title
    Type: USER
    Test: no need
    
    Change-Id: I6791e5476f997214e59b9b16fbcf07b5e2a3d590
    Signed-off-by: Vinod <vinod@oneplus.com>

--- Android.mk
+++ Android.mk
@@ -4,9 +4,9 @@ include $(CLEAR_VARS)
 
 TMP_LOCAL_PATH := $(LOCAL_PATH)
 
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-include $(TMP_LOCAL_PATH)/libbt-vendor/Android.mk
-endif # is-vendor-board-platform
+#ifeq ($(call is-vendor-board-platform,QCOM),true)
+#include $(TMP_LOCAL_PATH)/libbt-vendor/Android.mk
+#endif # is-vendor-board-platform
 
 include $(TMP_LOCAL_PATH)/tools/Android.mk
 
